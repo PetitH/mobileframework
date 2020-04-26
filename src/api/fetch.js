65bd59296d5a2dd27c0d4ed9ade06385b33df212
zhangjh <1107557408@qq.com>
2020-04-26 17:43:52 +0800

    更新封装requires

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,32 +1,16 @@
 //get
 function fetchGet(url, data) {
-  uni.request({
+  return uni.request({
     url: url,
     data: data
-  }).then(res => {
-    if (res.code != 200) {
-      return uni.showToast({
-        title: '接口错误',
-        duration: 2000
-      });
-    }
-    return res
   })
 }
 
 // post
 function fetchPost(url, data) {
-  uni.request({
+  return uni.request({
     url: url,
     data: data,
     method: 'post'
-  }).then(res => {
-    if (res.code != 200) {
-      return uni.showToast({
-        title: '接口错误',
-        duration: 2000
-      });
-    }
-    return res
   })
 }
